bd9b6ec93960fd56f698bc0745b59857a6147dbd
vavsn <41681585+vavsn@users.noreply.github.com>
2022-05-14 17:00:32 +0700

    Course project. Update 2. First webAPI.

--- CoureProject/Data/CoureProject.DAL.MSSQLServer/Migrations/20220514095252_Add_Field.Designer.cs
+++ CoureProject/Data/CoureProject.DAL.MSSQLServer/Migrations/20220514095252_Add_Field.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CoureProject.DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoureProject.DAL.SQLiteServer.Migrations
 {
     [DbContext(typeof(CoureProjectDB))]
-    partial class CoureProjectDBModelSnapshot : ModelSnapshot
+    [Migration("20220514095252_Add_Field")]
+    partial class Add_Field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.4");
---
+++ CoureProject/Services/CoureProject.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using CoureProject.Domain;
+using CoureProject.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoureProject.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/values")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IRepository<Consolidated_Weather> _weathers;
+        private readonly ILogger<ValuesController> _logger;
+        public ValuesController(IRepository<Consolidated_Weather> weathers, ILogger<ValuesController> logger)
+        {
+            _weathers = weathers;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var weathers = _weathers.GetAll();
+            return Ok(weathers);
+        }
+    }
+}
